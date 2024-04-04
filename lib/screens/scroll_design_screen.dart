488678fa499454d1dabdee6e16bd62a9d22322ff
Alejandro González Reyes <alexglzrys@gmail.com>
2024-04-04 17:14:41 -0600

    diseño con scroll - implementar widget PageView

--- lib/screens/scroll_design_screen.dart
+++ lib/screens/scroll_design_screen.dart
@@ -5,16 +5,53 @@ class ScrollDesignScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      // El diseño de esta pantalla requiere colocar elementos uno encima de otros en posiciones específicas
-      body: Stack(
-        children: [
-          // Imagen de fondo
-          _Background(),
-          // Contenido principal
-          _MainContent()
-        ],
+    return Scaffold(
+      body: Container(
+        // Agregar color gradiente al Container
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          // Listado de colores
+          colors: [
+            Color(0xff5ee8c5),
+            Color(0xff30bad6),
+          ],
+          // Posicion (orientación) inicial y final del degradado
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          // Puntos de parada para cada color declarado en el listado
+          stops: [0.5, 0.5],
+        )),
+        // PageView permite al usuario desplazarse horizontalmente o verticalmente entre varios widgets hijos.
+        // Es útil cuando deseas mostrar una colección de widgets de forma secuencial, donde cada widget representa una página o vista diferente.
+        child: PageView(
+          scrollDirection: Axis.vertical,
+          physics: const BouncingScrollPhysics(),
+          children: const [
+            _PrimaryView(),
+            _SecondaryView(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+// Widget referente a la vista principal (No. 1)
+class _PrimaryView extends StatelessWidget {
+  const _PrimaryView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // El diseño de esta pantalla requiere colocar elementos uno encima de otros en posiciones específicas
+    return const Stack(
+      children: [
+        // Imagen de fondo
+        _Background(),
+        // Contenido principal
+        _MainContent()
+      ],
     );
   }
 }
@@ -79,6 +116,55 @@ class _MainContent extends StatelessWidget {
               color: Colors.white,
               size: 80,
             )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// Widget referente a la vista secundaria (No. 2)
+class _SecondaryView extends StatelessWidget {
+  const _SecondaryView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      color: const Color(0xff30bad6),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Image(
+              image: AssetImage('assets/images/user.png'),
+              width: 200,
+              height: 200,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'Bienvenido a Flutter Page View, la única App que te ayuda a encontrar soluciones',
+              style: TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              // Decoración del botón
+              style: OutlinedButton.styleFrom(
+                  side: const BorderSide(color: Colors.white)),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'Entrar',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ),
+            ),
           ],
         ),
       ),
